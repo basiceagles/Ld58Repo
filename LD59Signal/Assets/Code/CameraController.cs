d4dfd5742972fdb18e7cc6961a76e585121b3cea
sSakuray <navixplay@mail.ru>
2026-04-18 15:13:56 +0300

    Movement+PickUp

---
+++ LD59Signal/Assets/Code/CameraController.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField] private float mouseSensitivity = 100f;
+    [SerializeField] private float minWaitAngle = -90f;
+    [SerializeField] private float maxWaitAngle = 90f;
+    [SerializeField] private Transform playerBody;
+
+    private float xRotation = 0f;
+
+    private void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    private void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, minWaitAngle, maxWaitAngle);
+
+        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+        playerBody.Rotate(Vector3.up * mouseX);
+    }
+}
