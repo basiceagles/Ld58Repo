e8c9b3313e9451f275cf341e56f57177bb77b40b
sSakuray <navixplay@mail.ru>
2026-04-19 18:14:19 +0300

    AntennaFix

--- LD59Signal/Assets/Code/AntennaController.cs
+++ LD59Signal/Assets/Code/AntennaController.cs
@@ -26,6 +26,7 @@ public class AntennaController : MonoBehaviour
     private float tilt;
     private Quaternion startRot;
     private AntennaController targetAntenna;
+    private ItemData itemData;
 
     private void Start()
     {
@@ -45,22 +46,40 @@ public class AntennaController : MonoBehaviour
         {
             lineRenderer.enabled = false;
         }
+
+        itemData = GetComponentInParent<ItemData>();
     }
 
     public void OnInteract(GameObject hit)
     {
-        if (IsPartOf(hit, buttonPart) && !isPowered)
+        if (IsPartOf(hit, buttonPart))
         {
-            isPowered = true;
-            if (humSource)
+            if (!isPowered)
             {
-                humSource.Play();
+                isPowered = true;
+                if (humSource)
+                {
+                    humSource.Play();
+                }
+                if (baseRenderer && baseRenderer.materials.Length > 1)
+                {
+                    Material[] mats = baseRenderer.materials;
+                    mats[1] = bulbOnMaterial;
+                    baseRenderer.materials = mats;
+                }
             }
-            if (baseRenderer && baseRenderer.materials.Length > 1)
+            else
             {
-                Material[] mats = baseRenderer.materials;
-                mats[1] = bulbOnMaterial;
-                baseRenderer.materials = mats;
+                // Unpower and fold
+                isPowered = false;
+                if (humSource) humSource.Stop();
+                if (baseRenderer && baseRenderer.materials.Length > 1)
+                {
+                    Material[] mats = baseRenderer.materials;
+                    mats[1] = null; 
+                    baseRenderer.materials = mats;
+                }
+                if (itemData) itemData.ToggleActivation(); 
             }
         }
         else if (IsPartOf(hit, monitorPart) && isPowered && !isViewing)
@@ -117,7 +136,6 @@ public class AntennaController : MonoBehaviour
         if (antennaCamera != null)
         {
             antennaCamera.enabled = false;
-            antennaCamera.transform.localRotation = startRot;
         }
 
         if (mainCam)
@@ -206,5 +224,13 @@ public class AntennaController : MonoBehaviour
         lineRenderer.positionCount = 2;
         lineRenderer.SetPosition(0, from);
         lineRenderer.SetPosition(1, to);
+    }
+
+    public void RotateDishManual(float input)
+    {
+        if (dishPart)
+        {
+            dishPart.transform.Rotate(Vector3.up, input * rotSpeed * Time.deltaTime, Space.World);
+        }
     }
 }
