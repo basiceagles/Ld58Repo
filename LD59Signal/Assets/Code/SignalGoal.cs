cdb2d41020e4f5d091b9653cdcc82ba5023ce69d
eagles_eagles <tankovarmata@gmail.com>
2026-04-20 17:50:08 +0300

    almost final

--- LD59Signal/Assets/Code/SignalGoal.cs
+++ LD59Signal/Assets/Code/SignalGoal.cs
@@ -1,10 +1,13 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class SignalGoal : MonoBehaviour
 {
+    public string sceneName;
     public Transform lineTarget;
     public void OnSignalReached()
     {
         Debug.Log("<color=green>ПОБЕДА! СИГНАЛ ДОШЕЛ ДО ЦЕЛИ!</color>");
+        SceneManager.LoadScene(sceneName);
     }
 }
