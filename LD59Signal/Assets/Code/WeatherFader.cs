af182622945344a7cbf9acde8bc8e7959603d992
eagles_eagles <tankovarmata@gmail.com>
2026-04-20 14:08:05 +0300

    ssaddsasad

--- LD59Signal/Assets/Code/WeatherFader.cs
+++ LD59Signal/Assets/Code/WeatherFader.cs
@@ -106,17 +106,9 @@ public sealed class WeatherFader : MonoBehaviour
         FadeToBasic();
     }
 
-    public void FadeToBasic()
-    {
-        FadeTo(WeatherType.Basic, _defaultFadeTimeSeconds);
-        StartCoroutine(CrossfadeAmbient(_basicAmbientClip, _audioFadeTimeSeconds));
-    }
+    public void FadeToBasic() => FadeTo(WeatherType.Basic, _defaultFadeTimeSeconds);
 
-    public void FadeToFoggy()
-    {
-        FadeTo(WeatherType.Foggy, _defaultFadeTimeSeconds);
-        StartCoroutine(CrossfadeAmbient(_foggyAmbientClip, _audioFadeTimeSeconds));
-    }
+    public void FadeToFoggy() => FadeTo(WeatherType.Foggy, _defaultFadeTimeSeconds);
 
     public void FadeToBasic(float fadeTimeSeconds) => FadeTo(WeatherType.Basic, fadeTimeSeconds);
 
