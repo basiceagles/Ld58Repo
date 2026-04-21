2a4fe2d7a72b04e214dceddffa42b9159609149a
Korzhik :3 <korzhik228.ar@gmail.com>
2026-04-21 18:02:34 +0300

    signalgoal добавлено условие

--- LD59Signal/Assets/Code/SignalGoal.cs
+++ LD59Signal/Assets/Code/SignalGoal.cs
@@ -3,11 +3,23 @@ using UnityEngine.SceneManagement;
 
 public class SignalGoal : MonoBehaviour
 {
-    public string sceneName;
+    [SerializeField] private string _sceneIfChanged;    // сцена если сигнал изменён
+    [SerializeField] private string _sceneIfNotChanged; // сцена если сигнал не изменён
+
     public Transform lineTarget;
+
     public void OnSignalReached()
     {
         Debug.Log("<color=green>ПОБЕДА! СИГНАЛ ДОШЕЛ ДО ЦЕЛИ!</color>");
-        SceneManager.LoadScene(sceneName);
+
+        bool changed = PlayerProgressManager.Instance != null
+                    && PlayerProgressManager.Instance.ChangedSignal;
+
+        string targetScene = changed ? _sceneIfChanged : _sceneIfNotChanged;
+
+        if (!string.IsNullOrEmpty(targetScene))
+            SceneManager.LoadScene(targetScene);
+        else
+            Debug.LogWarning("[SignalGoal] Имя сцены не назначено!", this);
     }
 }
